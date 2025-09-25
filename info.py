206da3c86d9b1b9536749ce1ba0665f322c7fc2f
Asbhaibsr <ffloverisarsad@gmail.com>
2025-09-25 20:56:18 +0530

    Update info.py

--- info.py
+++ info.py
@@ -22,7 +22,7 @@ ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ
 USERNAME = environ.get('USERNAME', "https://t.me/asbhaibsr") # ADMIN USERNAME
 LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1002352329534'))
 MOVIE_GROUP_LINK = environ.get('MOVIE_GROUP_LINK', 'https://t.me/all_movies_webseries_is_here')
-CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '-1002797023499 -1002463804038').split()]
+CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '-1002797023499').split()]
 #---------------------------------------------------------------
 #---------------------------------------------------------------
 DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://gofej47947_db_user:r9i9hoYTWvZyjS8W@cluster0.j1nxxou.mongodb.net/?retryWrites=true&w=majority&appName=Cluster029970536")
